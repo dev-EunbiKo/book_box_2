7a1adde3ee40fe2757fba9a1db9c455fe494aef9
dev_ebko <dev.ebko.ved@gmail.com>
2025-08-06 11:45:29 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/book_box_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.book_box_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      builder:
+          (context, child) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(colorSchemeSeed: Colors.pinkAccent),
+            title: 'screenutil test',
+            home: MyHomePage(title: 'Screen Util Test'),
+          ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen Util Test'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(width: 100, height: 100, color: Colors.blue),
+            Container(
+              width: ScreenUtil().setWidth(100),
+              height: ScreenUtil().setHeight(100),
+              color: Colors.red,
+            ),
+            Container(width: 100.w, height: 100.h, color: Colors.yellow),
+          ],
+        ),
+      ),
+    );
+  }
+}
