dff8b7a46691bda5db72e682fedad43505ac4e59
dev_ebko <dev.ebko.ved@gmail.com>
2025-08-13 22:15:52 +0900

    상수 파일 추가

---
+++ lib/core/constants.dart
@@ -0,0 +1,8 @@
+class BaseUrl {
+  static const String dataLibraryAPIUrl = 'http://data4library.kr/api';
+}
+
+class AuthKey {
+  static const String dataLibraryAuthKey =
+      '7755bd08a604e2449ad22506ea18e6147a191e4bef8f602bfff66b3092144a41';
+}
