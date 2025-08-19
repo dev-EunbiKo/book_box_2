119798dbc495cc7b9b0e2663de54b191740ebbb4
dev_ebko <dev.ebko.ved@gmail.com>
2025-08-19 16:22:32 +0900

    공통 로딩뷰 위젯 생성

---
+++ lib/core/component/custom_view/loading_view.dart
@@ -0,0 +1,36 @@
+import 'package:book_box_2/core/extensions/extension_object.dart';
+import 'package:book_box_2/core/navigation/navigation_service.dart';
+import 'package:flutter/material.dart';
+
+class LoadingView {
+  static OverlayEntry? overlayEntry;
+
+  static void show({BuildContext? context}) {
+    overlayEntry ??= OverlayEntry(
+      builder:
+          (BuildContext context) => Positioned.fill(
+            child: Container(
+              color: Colors.black.withValues(alpha: 0),
+              child: const Center(child: CircularProgressIndicator()),
+            ),
+          ),
+    );
+
+    if (context == null) {
+      NavigationService.navigatorKey.currentContext?.ifNotNull((buildContext) {
+        overlayEntry?.ifNotNull(
+          (overlay) => {Overlay.of(buildContext).insert(overlay)},
+        );
+      });
+    } else {
+      overlayEntry?.ifNotNull(
+        (overlay) => {Overlay.of(context).insert(overlay)},
+      );
+    }
+  }
+
+  static void hide() {
+    overlayEntry?.remove();
+    overlayEntry = null;
+  }
+}
