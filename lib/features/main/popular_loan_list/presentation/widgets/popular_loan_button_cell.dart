aa5c8cb6e1e18ea439bfb804ac365db0b2c61b0e
dev_ebko <dev.ebko.ved@gmail.com>
2025-08-21 19:24:02 +0900

    인기대여목록 cell 모양 수정

--- lib/features/main/popular_loan_list/presentation/widgets/popular_loan_button_cell.dart
+++ lib/features/main/popular_loan_list/presentation/widgets/popular_loan_button_cell.dart
@@ -1,17 +1,15 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:book_box_2/gen/assets.gen.dart';
+import 'package:book_box_2/core/utils.dart';
 import 'package:book_box_2/gen/colors.gen.dart';
 import 'package:book_box_2/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
-
 import 'package:book_box_2/data/model/data_library/popular_loan/select_popular_loan_list_data_model.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class PopularLoanButtonCell extends StatelessWidget {
   final SelPopularListData? data;
 
-  const PopularLoanButtonCell({super.key, this.data});
+  const PopularLoanButtonCell({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
@@ -23,17 +21,24 @@ class PopularLoanButtonCell extends StatelessWidget {
       },
       child: Row(
         children: [
+          Text(
+            data?.item?.no.toString() ?? '',
+            style: TextStyle(
+              fontFamily: BookBoxFontFamily.pretendardMedium,
+              fontSize: 14.sp,
+              color: BookBoxColor.c283a7d,
+            ),
+          ),
+          SizedBox(width: 11.w),
           Stack(
             alignment: Alignment.bottomCenter,
             children: [
               SizedBox(
-                width: 33.w,
+                width: 44.w,
                 child: AspectRatio(
                   aspectRatio: 2 / 3,
-                  // TODO: 캐시 이미지 구현
-                  child: SvgPicture.asset(
-                    data?.item?.bookImageURL ??
-                        BookBoxAssets.images.placeholder.path,
+                  child: Utils.getInstance().imageURlLoading(
+                    url: data?.item?.bookImageURL ?? '',
                     fit: BoxFit.cover,
                   ),
                 ),
