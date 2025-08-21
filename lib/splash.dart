bd14fd03236fedc2a803f226bbe76ef76d5189d8
dev_ebko <dev.ebko.ved@gmail.com>
2025-08-21 19:22:33 +0900

    splash 화면 전체화면 모드 추가

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,10 +1,17 @@
 import 'package:book_box_2/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  /// 상단바 다시 숨김
+  SystemChrome.setSystemUIChangeCallback((systemOverlaysAreVisible) async {
+    await Future.delayed(const Duration(seconds: 1));
+    SystemChrome.restoreSystemUIOverlays();
+  });
 
   await initialization();
 }
@@ -17,5 +24,20 @@ Future<void> initialization() async {
 /// 루팅, 업데이트 체크 후 runApp
 Future<void> appCheck() async {
   FlutterNativeSplash.remove();
+
+  /// 전체 화면 모드
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom],
+  );
+
+  /// 상단바투명 , 색상 검정
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
+
   return runApp(const MyApp());
 }
